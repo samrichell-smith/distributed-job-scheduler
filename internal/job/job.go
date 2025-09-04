50912a36ededc5bcbfed87a9fe855a902815e297
Sam Richell-Smith <samrs6012@gmail.com>
2025-09-04 16:59:54 +1200

    create job struct and status definitions

--- internal/job/job.go
+++ internal/job/job.go
@@ -1,1 +1,23 @@
 package job
+
+import "time"
+
+type Status string
+
+const (
+	Pending   Status = "Pending"
+	Running   Status = "Running"
+	Completed Status = "Completed"
+	Failed    Status = "Failed"
+)
+
+type Job struct {
+	ID          string
+	Name        string
+	Status      Status
+	Priority    int
+	Payload     interface{}
+	Result      interface{}
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
